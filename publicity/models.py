e08ffce3d35c9958880c3fba78a64a2ebbdaa1b5
julianab16 <julianamelis13@gmail.com>
2025-03-27 21:39:30 -0500

    added ticket models and publicity models

--- publicity/models.py
+++ publicity/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Publicity(models.Model):
+    id_publicity =  models.CharField(max_length=255, primary_key=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField(upload_to='publicidad_images/')
+    start_date = models.DateField()
+    end_date = models.DateField()
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = "publicity"
+        
+    def __str__(self):
+        return self.title
+
--- ticket/models.py
+++ ticket/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Ticket(models.Model):
+    # El ticket puede estar en proceso, cerrado o abierto
+    STATUS_CHOICES = [
+        ('open', 'Open'),
+        ('in_progress', 'In Progress'),
+        ('closed', 'Closed'),
+    ]
+    # El ticket puede tener prioridad bajo o alta
+    PRIORITY_CHOICES = [
+        ('low', 'Low'),
+        ('high', 'High'),
+    ]
+
+    id_ticket =  models.CharField(max_length=255, primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='open')
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES, default='low')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name='tickets_created')
+    assigned_to = models.ForeignKey('user.User', on_delete=models.SET_NULL, null=True, related_name='tickets_assigned')
+    punto_atencion = models.ForeignKey('PuntoAtencion', on_delete=models.SET_NULL, null=True)
+    
+    # La informacion que hay dentro del ticket
+    content = models.TextField()
+    class Meta:
+        db_table = "ticket"
+        
+    def __str__(self):
+        return self.title
