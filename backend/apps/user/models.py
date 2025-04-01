02ddda1df767d905ea61b0aa240a9d86d0e014c5
JohanACT <143613396+JohanACT@users.noreply.github.com>
2025-04-01 13:00:13 -0500

    add usuario model

--- backend/apps/user/models.py
+++ backend/apps/user/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Usuario(AbstractUser):
+    ADMINISTRADOR = 'ADMINISTRADOR'
+    CLIENTE = 'CLIENTE'
+    ROLES = [
+        (ADMINISTRADOR, 'Administrador'),
+        (CLIENTE, 'Cliente')
+    ]
+    
+    role = models.CharField(
+        max_length=15,
+        choices=ROLES,
+        default=CLIENTE,
+        verbose_name='Rol'
+    )
+    
+    phone_number = models.PositiveBigIntegerField(
+        unique=False,
+        blank=False,
+        null=False,
+        validators=[
+            RegexValidator(
+                regex=r'^(3|6)\d{9}$',
+                message='No es un número de teléfono válido',
+                code='invalid_phonenumber'
+            )
+        ],
+        verbose_name='Número teléfono'
+    )
+    
+    dni = models.PositiveBigIntegerField(
+        unique=True,
+        blank=False,
+        null=False,
+        validators=[
+            RegexValidator(
+                regex=r'^\d{7,10}$',
+                message='No es un número de documento válido',
+                code='invalid_dni'
+            )
+        ],
+        verbose_name='Cédula ciudadania'
+    )
+    
+    prioridad = models.BooleanField(
+        default=False,
+        verbose_name='¿Condición especial?'
+    )
+    
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'email', 'dni', 'phone_number', 'role']
+    
+    class Meta:
+        db_table = 'USUARIO'
+        verbose_name = 'Usuario'
+        verbose_name_plural = 'Usuarios'
+        ordering = ['id']
+    
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        self.first_name = self.first_name.upper()
+        self.last_name = self.last_name.upper()
+        self.email = self.email.lower()
+        super(Usuario, self).save(force_insert, force_update, using, update_fields)
