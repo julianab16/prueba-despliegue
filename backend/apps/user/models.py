ac002804ca890297688add9b4685bcd0794a2610
JohanACT <143613396+JohanACT@users.noreply.github.com>
2025-04-05 07:44:48 -0500

    custom model was defined inside settings.py

--- backend/apps/user/models.py
+++ backend/apps/user/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import AbstractUser
 
-
 class User(AbstractUser):
     ADMINISTRADOR = 'ADMINISTRADOR'
     CLIENTE = 'CLIENTE'
