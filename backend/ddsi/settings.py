b1551ed8517b6b7b08c734aec9ea4bf211fada12
julianab16 <julianamelis13@gmail.com>
2025-07-30 14:24:50 -0500

    agrega midd  a requirements

--- backend/ddsi/settings.py
+++ backend/ddsi/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 ROOT_URLCONF = 'ddsi.urls'
 
