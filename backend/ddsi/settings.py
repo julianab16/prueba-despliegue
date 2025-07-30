f3aa1c375afd296be5a0c683b22ce2a3fac59aac
julianab16 <julianamelis13@gmail.com>
2025-07-30 15:20:52 -0500

    agrega apps.  a requirements

--- backend/ddsi/settings.py
+++ backend/ddsi/settings.py
@@ -18,7 +18,7 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-AUTH_USER_MODEL = 'user.User'
+AUTH_USER_MODEL = 'apps.user.User'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
