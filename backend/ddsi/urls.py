4df8d4dab760d2904f7bebdfa2a8291b07728c8d
julianab16 <julianamelis13@gmail.com>
2025-07-30 16:12:17 -0500

    agrega admin url default

--- backend/ddsi/urls.py
+++ backend/ddsi/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from apps.user import urls as user_urls
+from django.contrib import admin
+from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('/admin/')), 
     path('admin/', admin.site.urls),
     path('ticket/', include('apps.ticket.urls')), 
     path('api/attention-points/', include('apps.attention_point.urls')),   
