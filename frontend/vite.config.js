a2ae794f4b4bf11196e5f17bffb8613689640dd1
julianab16 <julianamelis13@gmail.com>
2025-07-31 01:36:39 -0500

    Agrega historyApiFallback a vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     outDir: 'dist'
   },
   server: {
-    port: 5173
+    historyApiFallback: true,
   },
   base: '/', // ← Esto es importante para que el deploy funcione
 })
